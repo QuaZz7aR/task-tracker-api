febacb68237f8834a8faa708af1b02647a4f49d7
QuaZz7aR <quazzz13@gmail.com>
2026-08-20 15:07:05 +0200

    feat: basic express functionality, zod schema and  docker compose file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello world")
+import {pool} from './db.js'
+import express from 'express'
+import {z} from 'zod'
+
+const registeredSchema = z.object({
+ email: z.email(),
+ password: z.string().min(8),
+})
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+ res.send('Server is running');
+})
+
+
+
+app.listen(PORT, () => {
+ console.log(`Server listening on ${PORT}`);
+})
